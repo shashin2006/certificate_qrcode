3c03ef84cf0dde7972bcebb178b927d363d1e7c1
shashin2006 <shashinrajkumar@gmail.com>
2026-02-26 23:14:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ const path=require('path')
 const Certificate = require('./models/Certificate');
 const { generateCertificateId, generateToken } = require('./utils/generateId');
 const createCertificate = require('./generatecertificate');
+const cloudinary = require('cloudinary').v2;
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 // =============================
 // CONFIGURATION
 // =============================
